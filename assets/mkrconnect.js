d377c35b59c69c8f6cd90cb8d7530fa8cff3c040
Steven Burns <steven@stevenburns.net>
2019-08-30 11:53:56 -0400

    Updates for new MKR Board - Connection Test

--- assets/mkrconnect.js
+++ assets/mkrconnect.js
@@ -1,1 +1,19 @@
-alert('hello world');
+console.log('Connected to API View Service');
+
+$.ajax(
+    {
+        "url": "http://homenet.stevenburns.net",
+        "type": "GET",
+        "dataType": "HTML",
+        "crossDomain": true,
+        success: function (res) {
+            $(body).append(res);
+            //res = JSON.parse(res);
+            //console.log(res.html);
+
+        },
+        error: function (res) {
+            console.error("ERROR!");
+            console.log(res);
+        }
+    });
